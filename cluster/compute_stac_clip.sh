0c5d6af77c3d982ab7e9097c604e33a316712749
diegoaldarondo <diegoaldarondo@g.harvard.edu>
2020-05-15 11:19:11 -0400

    Sync multiple repos

--- cluster/compute_stac_clip.sh
+++ cluster/compute_stac_clip.sh
@@ -4,15 +4,5 @@ export img_path="/n/home02/daldarondo/LabDir/Diego/.images/mj_stac.sif"
 export MUJOCO_GL="osmesa"
 export MJLIB_PATH="/home/.mujoco/mujoco200_linux/bin/libmujoco200.so"
 export MJKEY_PATH="/home/.mujoco/mjkey.txt"
-<<<<<<< HEAD
 stac_path="/n/holylfs02/LABS/olveczky_lab/Diego/code/dm/stac"
 python3 $stac_path/batch_compute_stac_clip_singularity.py
-=======
-cd /home/code/stac
-data_path=$1
-param_path=$2
-save_path=$3
-offset_path=$4
-start_frame=$5
-python3 compute_stac.py $data_path $param_path --save-path=$save_path --offset-path=$offset_path --start-frame=$start_frame --verbose="True" --process-snippet="False" --skip=1
->>>>>>> 493409a8c72119d325f865113f7962d6ca15e888
--- cluster/submit_compute_stac_clip.sh
+++ cluster/submit_compute_stac_clip.sh
@@ -2,13 +2,8 @@
 #SBATCH -J compute_stac
 #SBATCH -N 1                # number of nodes
 #SBATCH -n 1                # number of tasks
-<<<<<<< HEAD
 #SBATCH -c 1               # Number of threads (cores)
 #SBATCH --mem 15000        # memory for all cores
-=======
-#SBATCH -c 8               # Number of threads (cores)
-#SBATCH --mem 20000        # memory for all cores
->>>>>>> 493409a8c72119d325f865113f7962d6ca15e888
 #SBATCH -t 0-05:00          # time (D-HH:MM)
 #SBATCH --export=ALL
 #SBATCH -o logs/Job.compute_stac.%N.%j.out    # STDOUT
