c2faa1d831f6db230f7b6df0e47fbd94e3f3498e
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2020-12-03 13:43:08 -0500

    Black and make more memory efficient

--- merge_snippets.py
+++ merge_snippets.py
@@ -3,6 +3,8 @@ import numpy as np
 import pickle
 import clize
 import os
+import argparse
+from scipy.io import savemat
 
 
 def _sort_fn(file):
@@ -37,27 +39,48 @@ def merge(folder, *, save_path=None):
 
     n_frames = q.shape[0]
     remainder = q_rem.shape[0]
-    q = np.zeros((n_frames * (len(files) - 1) + remainder, q.shape[1]))
-    x = np.zeros((n_frames * (len(files) - 1) + remainder, x.shape[1], x.shape[2]))
-    kp_data = np.zeros((n_frames * (len(files) - 1) + remainder, kp_data.shape[1]))
+    out_dict = in_dict
+    out_dict["qpos"] = np.zeros(
+        (n_frames * (len(files) - 1) + remainder, q.shape[1])
+    )
+    out_dict["xpos"] = np.zeros(
+        (n_frames * (len(files) - 1) + remainder, x.shape[1], x.shape[2])
+    )
+    out_dict["kp_data"] = np.zeros(
+        (n_frames * (len(files) - 1) + remainder, kp_data.shape[1])
+    )
 
     for i, file in enumerate(files):
         print("%d of %d" % (i, len(files)), flush=True)
         q_i, x_i, kp_data_i, _ = get_chunk_data(os.path.join(folder, file))
         start_frame = i * n_frames
-        end_frame = np.min([((i + 1) * n_frames), q.shape[0]])
-        q[start_frame:end_frame, ...] = q_i
-        x[start_frame:end_frame, ...] = x_i
-        kp_data[start_frame:end_frame, ...] = kp_data_i
+        end_frame = np.min([((i + 1) * n_frames), out_dict["qpos"].shape[0]])
+        out_dict["qpos"][start_frame:end_frame, ...] = q_i
+        out_dict["xpos"][start_frame:end_frame, ...] = x_i
+        out_dict["kp_data"][start_frame:end_frame, ...] = kp_data_i
 
-    out_dict = in_dict
-    out_dict["qpos"] = q
-    out_dict["xpos"] = x
-    out_dict["kp_data"] = kp_data
     if save_path is None:
         save_path = os.path.join(folder, "total.p")
     with open(save_path, "wb") as f:
         pickle.dump(out_dict, f, protocol=4)
+    mat_path = save_path.split(".p")[0] + ".mat"
+    for k, v in out_dict.items():
+        if v is None:
+            out_dict[k] = "None"
+    savemat(mat_path, out_dict)
+
+
+def stac_merge():
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument(
+        "stac_folder",
+        help="Path to stac folder containing stacced snippets.",
+    )
+    args = parser.parse_args()
+    merge(args.stac_folder)
 
 
 if __name__ == "__main__":
