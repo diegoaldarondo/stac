ecf50d6a3a494cbc4d05a70897eb81184509ae82
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2021-04-03 16:54:38 -0400

    Add documentation and change the way arenas are constructed

--- stac/view_stac.py
+++ stac/view_stac.py
@@ -1,4 +1,8 @@
-"""View stac results."""
+"""View stac results.
+
+Attributes:
+    FPS (int): Default frames per second of the video. 
+"""
 from dm_control import viewer
 from dm_control.locomotion.walkers import rescale
 import clize
@@ -10,11 +14,25 @@ import os
 import imageio
 from dm_control.mujoco import wrapper
 from dm_control.mujoco.wrapper.mjbindings import enums
-
+from typing import Text, List, Dict, Union, Tuple
 
 FPS = 50
 
-def load_data(data_path, start_frame=0, end_frame=-1):
+
+def load_data(data_path: Text, start_frame: int = 0, end_frame: int = -1):
+    """Load data from .pickle file
+
+    Args:
+        data_path (TYPE): Path to .pickle file contatining qpos
+        start_frame (int, optional): First frame
+        end_frame (TYPE, optional): Last frame
+
+    Returns:
+        TYPE: Qpos List, site offsets, kp_data np.ndarray, number of frames
+
+    Raises:
+        ValueError: Description
+    """
     with open(data_path, "rb") as f:
         in_dict = pickle.load(f)
         q = in_dict["qpos"]
@@ -35,22 +53,25 @@ def load_data(data_path, start_frame=0, end_frame=-1):
 
 
 def view_stac(
-    data_path,
-    param_path,
+    data_path: Text,
+    param_path: Text,
     *,
-    render_video=False,
-    save_path=None,
-    headless=False,
-    start_frame=0,
-    end_frame=-1
+    render_video: bool = False,
+    save_path: Text = None,
+    headless: bool = False,
+    start_frame: int = 0,
+    end_frame: int = -1
 ):
     """View the output of stac.
 
-    :param data_path: Path to .p file containing qpos, offsets,
-                      and optionally kp_data.
-    :param render_video: If True, make a video and put it in clips.
-    :param save_path: Save any rendered videos to this path.
-    :param headless: If True, make a video in headless mode.
+    Args:
+        data_path (Text): Path to .p file containing qpos, offsets, and optionally kp_data.
+        param_path (Text): Path to stac parameters file.
+        render_video (bool, optional): If True, make a video and put it in clips.
+        save_path (Text, optional): Save any rendered videos to this path.
+        headless (bool, optional): If True, make a video in headless mode.
+        start_frame (int, optional): Starting frame for rendering.
+        end_frame (int, optional): Ending frame for rendering.
     """
     # Load in the qpos, offsets, and markers
     q, offsets, kp_data, n_frames = load_data(
@@ -70,42 +91,60 @@ def view_stac(
 
 
 def setup_visualization(
-    param_path,
+    param_path: str,
     q,
     offsets,
     kp_data,
-    n_frames,
-    render_video=False,
-    save_path=None,
-    headless=False,
+    n_frames: int,
+    render_video: bool = False,
+    save_path: Text = None,
+    headless: bool = False,
 ):
+    """Sets up stac visualization.
+
+    Args:
+        param_path (str): Path to stac parameters file.
+        q (TYPE): Array of qpos for each frames.
+        offsets (TYPE): Site offsets
+        kp_data (TYPE): Description
+        n_frames (TYPE): Number of frames
+        render_video (bool, optional): If True, make a video and put it in clips.
+        save_path (Text, optional): Save any rendered videos to this path.
+        headless (bool, optional): If True, make a video in headless mode.
+    """
     params = util.load_params(param_path)
     params["n_frames"] = n_frames - 1
 
     # Build the environment, and set the offsets, and params
-    params[
-        "data_path"
-    ] = "/home/diego/data/dm/stac/snippets/snippets_snippet_v8_JDM31_Day_8/reformatted/snippet_3_AdjustPosture.mat"
-    if params["_USE_HFIELD"]:
+    if params["ARENA_TYPE"] == "HField":
+        params[
+            "data_path"
+        ] = "/home/diego/data/dm/stac/snippets/snippets_snippet_v8_JDM31_Day_8/reformatted/snippet_3_AdjustPosture.mat"
         env = rodent_environments.rodent_mocap(
             kp_data,
             params,
-            use_hfield=params["_USE_HFIELD"],
             # hfield_image=in_dict['hfield_image'],
             hfield_image=None,
             pedestal_center=in_dict["pedestal_center"],
             pedestal_radius=in_dict["pedestal_radius"],
             pedestal_height=in_dict["pedestal_height"],
-            arena_diameter=in_dict["scaled_arena_diameter"],
+            arena_diameter=in_dict["_ARENA_DIAMETER"],
+            arena_center=in_dict["_ARENA_CENTER"],
         )
-    else:
+    elif params["ARENA_TYPE"] == "Standard":
+        env = rodent_environments.rodent_mocap(kp_data, params)
+    elif params["ARENA_TYPE"] == "DannceArena":
         env = rodent_environments.rodent_mocap(
-            kp_data, params, use_hfield=params["_USE_HFIELD"]
+            kp_data,
+            params,
+            arena_diameter=params["_ARENA_DIAMETER"],
+            arena_center=params["_ARENA_CENTER"],
         )
-    # import pdb;
-    # pdb.set_trace()
+
     rescale.rescale_subtree(
-        env.task._walker._mjcf_root, params["scale_factor"], params["scale_factor"]
+        env.task._walker._mjcf_root,
+        params["scale_factor"],
+        params["scale_factor"],
     )
     env.task._arena._mjcf_root.worldbody.add(
         "camera",
@@ -146,6 +185,7 @@ def setup_visualization(
             )
             frames.append(rgbArr)
             n_frame += 1
+            print(n_frame)
             prev_time = np.round(env.physics.time(), decimals=2)
     # Otherwise, use the viewer
     else:
@@ -158,14 +198,22 @@ def setup_visualization(
         print("Rendering: ", env.task.video_name)
         write_video(save_path, frames)
 
-def write_video(filename, frames, fps=5):
-        """Write a video to disk"""
-        if not os.path.exists(os.path.dirname(filename)):
-            os.makedirs(os.path.dirname(filename))
-        print("Writing to %s" % (filename))
-        with imageio.get_writer(filename, fps=fps) as video:
-            for frame in frames:
-                video.append_data(frame)
+
+def write_video(filename: Text, frames: List, fps: int = FPS):
+    """Write a video to disk
+
+    Args:
+        filename (Text): File name in which to save video
+        frames (List): List of frames to include in video
+        fps (int, optional): Frames per second.
+    """
+    if not os.path.exists(os.path.dirname(filename)):
+        os.makedirs(os.path.dirname(filename))
+    print("Writing to %s" % (filename))
+    with imageio.get_writer(filename, fps=fps) as video:
+        for frame in frames:
+            video.append_data(frame)
+
 
 if __name__ == "__main__":
     clize.run(view_stac)
