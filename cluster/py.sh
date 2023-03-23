59e26ed2e2ed5426904a5736925de54eb1b4ec87
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2023-03-23 12:34:42 -0400

    remove unnecessary scripts

--- cluster/py.sh
+++
@@ -1,5 +0,0 @@
-echo "$@"
-module load python/3.6.3-fasrc02
-module load bazel/0.13.0-fasrc01 gcc/4.9.3-fasrc01 hdf5/1.8.12-fasrc08 cmake
-source ~/.envs/mujoco200_python3.6/bin/activate
-python "$@"
