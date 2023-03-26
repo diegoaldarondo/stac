c2c9186555006697dd31ddffbc54e7d4faa6755d
Diego Aldarondo <diegoaldarondo@g.harvard.edu>
2023-03-26 15:15:53 -0400

    Remove snippet related functions and add loadmat wrapper

--- stac/util.py
+++ stac/util.py
@@ -3,7 +3,43 @@ import numpy as np
 import h5py
 import os
 import yaml
-from scipy.io import loadmat
+import scipy.io as spio
+
+
+def loadmat(filename):
+    """
+    this function should be called instead of direct spio.loadmat
+    as it cures the problem of not properly recovering python dictionaries
+    from mat files. It calls the function check keys to cure all entries
+    which are still mat-objects
+    """
+    data = spio.loadmat(filename, struct_as_record=False, squeeze_me=True)
+    return _check_keys(data)
+
+
+def _check_keys(dict):
+    """
+    checks if entries in dictionary are mat-objects. If yes
+    todict is called to change them to nested dictionaries
+    """
+    for key in dict:
+        if isinstance(dict[key], spio.matlab.mio5_params.mat_struct):
+            dict[key] = _todict(dict[key])
+    return dict
+
+
+def _todict(matobj):
+    """
+    A recursive function which constructs from matobjects nested dictionaries
+    """
+    dict = {}
+    for strg in matobj._fieldnames:
+        elem = matobj.__dict__[strg]
+        if isinstance(elem, spio.matlab.mio5_params.mat_struct):
+            dict[strg] = _todict(elem)
+        else:
+            dict[strg] = elem
+    return dict
 
 
 def load_params(param_path):
@@ -17,96 +53,3 @@ def load_params(param_path):
         except yaml.YAMLError as exc:
             print(exc)
     return params
-
-
-def load_dannce_data(filename, skeleton_file=None, start_frame=None, end_frame=None):
-    data = loadmat(filename)
-    skeleton = loadmat(skeleton_file)
-    kp_names = skeleton["joint_names"]
-    kp_names = [name[0] for name in kp_names[0]]
-    order = np.argsort(kp_names)
-
-    kp_data = data["pred"][:]
-    kp_data = kp_data[start_frame:end_frame, ...]
-    kp_data = kp_data[..., order]
-
-    kp_data = np.transpose(kp_data, (0, 2, 1))
-    kp_data = np.reshape(kp_data, (kp_data.shape[0], -1))
-    return kp_data, kp_names
-
-
-def load_kp_data_from_file(
-    filename, struct_name="markers_preproc", start_frame=None, end_frame=None
-):
-    """Format kp_data files from matlab to python through hdf5.
-
-    :param filename: Path to v7.3 mat file containing
-    :param struct_name: Name of the struct to load.
-    """
-    try:
-        with h5py.File(filename, "r") as f:
-            data = f["mocapstruct_here"][struct_name]
-            kp_names = [k for k in data.keys()]
-            print(kp_names, flush=True)
-
-            # Concatenate the data for each keypoint, and format to (t x n_dims)
-            if start_frame is None:
-                kp_data = np.concatenate([data[name][:] for name in kp_names]).T
-            else:
-                markers = []
-                for name in kp_names:
-                    print(name, flush=True)
-                    m = data[name][:]
-                    markers.append(m[:, start_frame:end_frame])
-                kp_data = np.concatenate(markers).T
-    except OSError:
-        data = loadmat(filename)
-        data = data["predictions"]
-        kp_names = [k for k in data.dtype.names if not any([n in k for n in ["Shin"]])]
-        kp_names.sort()
-        if start_frame is None:
-            kp_data = np.concatenate([data[name][0, 0] for name in kp_names], axis=1)
-        else:
-            kp_data = np.concatenate(
-                [data[name][0, 0][start_frame:end_frame, :] for name in kp_names],
-                axis=1,
-            )
-    return kp_data, kp_names
-
-
-def load_snippets_from_file(filename):
-    """Load snippet."""
-    with h5py.File(filename, "r") as f:
-        try:
-            data = f["data"]
-        except KeyError:
-            try:
-                data = f["preproc_mocap"]
-            except KeyError:
-                data = f["markers_preproc"]
-        kp_names = [k for k in data.keys()]
-        # Concatenate the data for each keypoint,
-        # and format to (t x n_dims)
-        snippet = np.concatenate([data[name][:] for name in kp_names]).T
-        behavior = "".join(chr(x) for x in f["clustername"][:])
-        # com_vel = f['snippet_com_vel'][:][0]
-        com_vel = 0
-    return snippet, kp_names, behavior, com_vel
-
-
-def load_snippets_from_folder(folder):
-    """Load snippets from file and return list of kp_data."""
-    files = [
-        f
-        for f in os.listdir(folder)
-        if os.path.isfile(os.path.join(folder, f)) and "params" not in f
-    ]
-    filenames = [os.path.join(folder, f) for f in files]
-    snippets = [None] * len(filenames)
-    behaviors = [None] * len(filenames)
-    com_vels = [None] * len(filenames)
-    for i, filename in enumerate(filenames):
-        snippets[i], kp_names, behaviors[i], com_vels[i] = load_snippets_from_file(
-            filename
-        )
-    return snippets, kp_names, files, behaviors, com_vels
